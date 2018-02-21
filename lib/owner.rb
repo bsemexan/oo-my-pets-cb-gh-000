12733d864c84e4787f827cfd72d88998785e5135
Berry  Semexan <bsemexan@gmail.com>
2018-02-21 02:59:23 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,24 @@
 class Owner
   # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+  @@all = []
+
+  def initialize(species)
+    @species = species
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def say_species
+    puts "#{@species}"
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
 end
