9a8fd0e5c1cd36420d6ca75476e5ecd8ebd2942e
Berry  Semexan <bsemexan@gmail.com>
2018-02-21 03:30:51 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,6 +61,6 @@ class Owner
   end
 
   def list_pets
-    "I have #{@pets[:dogs].count} fish, #{} dog(s), and  cat(s)."
+    "I have #{@pets[:fishes].count} fish, #{} dog(s), and  cat(s)."
   end
 end
